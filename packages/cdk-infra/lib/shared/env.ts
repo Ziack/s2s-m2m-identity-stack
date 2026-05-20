392aad2e76b3c2215abb6865c5a36343303e9e71
Garcia Altamiranda, Â Javier Alfonso <javier.garcia@duckcreek.com>
2026-05-20 14:38:22 -0500

    chore(infra): remove CDK package

--- packages/cdk-infra/lib/shared/env.ts
+++
@@ -1,8 +0,0 @@
-import { Environment } from 'aws-cdk-lib';
-
-export function devEnv(): Environment {
-  return {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION ?? 'us-east-1',
-  };
-}
